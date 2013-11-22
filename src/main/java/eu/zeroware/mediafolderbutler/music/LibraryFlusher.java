821c8fd3e6841109022c2ee62359315d525ffeff
giancarlo lallopizzi <giancarlolallopizzi@gmail.com>
2013-11-22 17:49:06 +0100

    MediaFilesWriter created

--- src/main/java/eu/zeroware/mediafolderbutler/music/LibraryFlusher.java
+++ src/main/java/eu/zeroware/mediafolderbutler/music/LibraryFlusher.java
@@ -8,51 +8,37 @@ import org.apache.commons.io.FileUtils;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
+import com.google.inject.Inject;
+
 import eu.zeroware.mediafolderbutler.music.entity.Album;
 import eu.zeroware.mediafolderbutler.music.entity.Artist;
 import eu.zeroware.mediafolderbutler.music.entity.Song;
+import eu.zeroware.mediafolderbutler.utils.MediaFilesWriter;
 
+public class LibraryFlusher extends MediaLibraryWorker {
 
-public class LibraryFlusher extends MediaLibraryWorker{
-	
 	private static Logger logger = LogManager.getLogger(LibraryFlusher.class);
 	private final String NEW_ROOT_FOLDER = FOLDER_SEPARATOR + "newroot";
 	private List<File> skippedFiles;
-	
-	
-	public void flush(){
+
+	@Inject
+	private MediaFilesWriter mediaFilesWriter;
+
+	public void flush() {
 		flushLibrary();
 		flushSkippedFiles();
 	}
-	
-	
-	private void flushLibrary(){
+
+	private void flushLibrary() {
 		logger.info("Flush library here..");
-		if(makeNewDestinationFolder()){
+		if (makeNewDestinationFolder()) {
 			for (Artist artist : library.getArtists()) {
-				if(makeArtistFolder(artist)){
+				if (makeArtistFolder(artist)) {
 					for (Album album : library.getAlbums()) {
-						if(album.getArtist().getName().equals(artist.getName())){
-							if(makeAlbumFolder(artist, album)){
+						if (album.getArtist().getName().equals(artist.getName())) {
+							if (makeAlbumFolder(artist, album)) {
 								for (Song song : album.getTracks()) {
-									String destFilename = song.getName() + "." + song.getAudioFileType();
-									String absoluteDestFilePath = getAlbumFolderName(artist, album) + FOLDER_SEPARATOR + destFilename;
-									logger.info("Now copy " + song.getFile().getAbsolutePath() + " to its new position: " + absoluteDestFilePath);
-									
-									
-									if(debug){
-										makeFile(absoluteDestFilePath);
-										
-									}else{
-										File srcFile = song.getFile();
-										File destFile = new File(absoluteDestFilePath);
-										
-										try {
-											 FileUtils.moveFile(srcFile, destFile);
-										} catch (IOException e) {
-											logger.error("Error during copy of " + song.getFile().getAbsolutePath() + " on its new position " + absoluteDestFilePath, e);
-										}	
-									}
+									mediaFilesWriter.writeSongToFolder(song, getAlbumFolderName(artist, album));
 								}
 							}
 						}
@@ -61,47 +47,39 @@ public class LibraryFlusher extends MediaLibraryWorker{
 			}
 		}
 	}
-	
-	private void flushSkippedFiles(){
+
+	private void flushSkippedFiles() {
 		for (File skippedItem : getSkippedFiles()) {
 			logger.info("SkippedItem: " + skippedItem.getAbsolutePath());
 		}
 	}
-	
-	
-	
 
-	private boolean makeAlbumFolder(Artist artist, Album album){
+	private boolean makeAlbumFolder(Artist artist, Album album) {
 		return makeFolder(getAlbumFolderName(artist, album));
 	}
-	
-	private String getAlbumFolderName(Artist artist, Album album){
-		return getArtistFolderName(artist) + FOLDER_SEPARATOR + artist.getName() + " - " + album.getName() + " (" + album.getYear() + ")";
+
+	private String getAlbumFolderName(Artist artist, Album album) {
+		return getArtistFolderName(artist) + FOLDER_SEPARATOR + artist.getName() + " - " + album.getName() + " ("
+				+ album.getYear() + ")";
 	}
-	
-	
-	
-	private boolean makeArtistFolder(Artist artist){
+
+	private boolean makeArtistFolder(Artist artist) {
 		return makeFolder(getArtistFolderName(artist));
 	}
-	
-	private String getArtistFolderName(Artist artist){
+
+	private String getArtistFolderName(Artist artist) {
 		return getNewDestinationFolderName() + FOLDER_SEPARATOR + artist.getName();
 	}
-	
-	
-	
-	private boolean makeNewDestinationFolder(){
+
+	private boolean makeNewDestinationFolder() {
 		return makeFolder(getNewDestinationFolderName());
 	}
-	
-	private String getNewDestinationFolderName(){
+
+	private String getNewDestinationFolderName() {
 		return BASE_MUSIC_FOLDER + NEW_ROOT_FOLDER;
 	}
-	
-	
-	
-	private boolean makeFile(String fileToCreate){
+
+	private boolean makeFile(String fileToCreate) {
 		File newFolder = new File(fileToCreate);
 		try {
 			FileUtils.touch(newFolder);
@@ -111,8 +89,8 @@ public class LibraryFlusher extends MediaLibraryWorker{
 		}
 		return true;
 	}
-	
-	private boolean makeFolder(String folderToCreate){
+
+	private boolean makeFolder(String folderToCreate) {
 		File newFolder = new File(folderToCreate);
 		try {
 			newFolder.mkdir();
@@ -132,10 +110,9 @@ public class LibraryFlusher extends MediaLibraryWorker{
 		return skippedFiles;
 	}
 
-
 	@Override
 	public void print() {
 		// TODO Auto-generated method stub
 	}
-	
+
 }
---
+++ src/main/java/eu/zeroware/mediafolderbutler/utils/MediaFilesWriter.java
@@ -0,0 +1,82 @@
+package eu.zeroware.mediafolderbutler.utils;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+import com.google.inject.Inject;
+import com.google.inject.name.Named;
+
+import eu.zeroware.mediafolderbutler.entity.Creation;
+import eu.zeroware.mediafolderbutler.music.entity.Song;
+
+public class MediaFilesWriter {
+
+	@Inject
+	@Named("debug")
+	protected Boolean debug;
+
+	private static Logger logger = LogManager.getLogger(MediaFilesWriter.class);
+	private final String FOLDER_SEPARATOR = System.getProperty("file.separator");
+
+	public void writeSongToFolder(Song song, String folder) {
+
+		String destFilename = song.getName() + "." + song.getAudioFileType();
+		String absoluteDestFilePath = folder + FOLDER_SEPARATOR + destFilename;
+
+		logger.info("Now copy " + song.getFile().getAbsolutePath() + " to its new position: " + absoluteDestFilePath);
+
+		if (debug) {
+			makeFile(absoluteDestFilePath);
+
+		} else {
+			File srcFile = song.getFile();
+			File destFile = new File(absoluteDestFilePath);
+
+			try {
+				FileUtils.moveFile(srcFile, destFile);
+			} catch (IOException e) {
+				logger.error("Error during copy of " + song.getFile().getAbsolutePath() + " on its new position "
+						+ absoluteDestFilePath, e);
+			}
+		}
+	}
+
+	public void writeMediaFileToFolder(File file, String folder) {
+	}
+
+	public void writeMediaElementToFolder(Creation mediaElement, String folder) {
+	}
+
+	public void flushSongElementToRootFolder(Song song, String rooFolder) {
+		String artist = song.getArtist();
+		String album = song.getAlbum();
+
+	}
+
+	public boolean makeFolder(String folderToCreate) {
+		File newFolder = new File(folderToCreate);
+		try {
+			newFolder.mkdir();
+			FileUtils.touch(newFolder);
+			logger.info("New folder created on: " + newFolder.getAbsolutePath());
+		} catch (IOException e) {
+			return false;
+		}
+		return true;
+	}
+
+	public boolean makeFile(String fileToCreate) {
+		File newFolder = new File(fileToCreate);
+		try {
+			FileUtils.touch(newFolder);
+			logger.info("New file created on: " + newFolder.getAbsolutePath());
+		} catch (IOException e) {
+			return false;
+		}
+		return true;
+	}
+}
