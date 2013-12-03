38b05c394a0b2593499214726e906f2341a2294c
glallopitzi <giancarlolallopizzi@gmail.com>
2013-12-03 23:37:26 +0100

    movie entity refactoring

--- src/main/java/eu/zeroware/mediafolderbutler/movie/entity/Director.java
+++ src/main/java/eu/zeroware/mediafolderbutler/movie/entity/Director.java
@@ -1,123 +1,33 @@
 package eu.zeroware.mediafolderbutler.movie.entity;
 
-import java.util.ArrayList;
-import java.util.List;
+import eu.zeroware.mediafolderbutler.entity.Artist;
 
-import eu.zeroware.mediafolderbutler.utils.rest.musicbrainz.search.ArtistSearchResultItem;
-
-public class Director {
-	private String name;
-	private List<Album> albums;
-	private List<Movie> movies;
-	
-	private List<String> genres;
-	private List<String> tags;
-	
-	private String mbid;
-	
-	public Director() {
-	}
+public class Director extends Artist {
+		
+	public Director() {}
 
 	public Director(Movie movie) {
-		this.name = movie.getArtist();
-		movies = new ArrayList<Movie>();
-		movies.add(movie);
-		albums = new ArrayList<Album>();
-		albums.add(new Album(movie.getAlbum(), movie.getArtist()));
+		super(movie);
 	}
 	
 	public Director(String name) {
-		this.name = name;
-	}
-
-	public Director(ArtistSearchResultItem artistSearchResultItem) {
-		this.name = artistSearchResultItem.getName();
+		super(name);
 	}
 
 	public void addSong(Movie movie){
-		if(!movies.contains(movie)){
-			movies.add(movie);
-		}
-		
-		addAlbum(movie);
+		super.addCreation(movie);
 	}
 	
 	public void addAlbum(Movie movie){
-		Album album = new Album(movie.getAlbum(), movie.getArtist());
-		if(!albums.contains(album)){
-			albums.add(album);
-		}
+		super.addCollection(movie);
 	}
 	
-	@Override
-	public boolean equals(Object obj) {
-		Director o = (Director) obj;
-		try {
-			return this.name.equals(o.getName());
-		} catch (Exception e) {
-			// logger.warn
-			return false;
-		}
-
-	}
 	
 	@Override
 	public String toString() {
-		StringBuilder builder = new StringBuilder();
-		builder.append("Artist info" + "\n");
-		builder.append("------------" + "\n");
-		builder.append("Name: " + name + "\n");
-		if(albums != null)
-			builder.append("Albums number: " + albums.size() + "\n");
-		builder.append("------------" + "\n");
-		return builder.toString();
+		String toString = super.toString();
+		// TODO replace Artist with Director and Collections with ??
+		return toString;
 	}
 
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public List<Album> getAlbums() {
-		return albums;
-	}
-
-	public void setAlbums(List<Album> albums) {
-		this.albums = albums;
-	}
-
-	public List<String> getTags() {
-		return tags;
-	}
-
-	public void setTags(List<String> tags) {
-		this.tags = tags;
-	}
-
-	public void setGenres(List<String> genres) {
-		this.genres = genres;
-	}
-
-	public List<String> getGenres() {
-		return genres;
-	}
-
-	public void setMbid(String mbid) {
-		this.mbid = mbid;
-	}
-
-	public String getMbid() {
-		return mbid;
-	}
-
-	public void setSongs(List<Movie> movies) {
-		this.movies = movies;
-	}
-
-	public List<Movie> getSongs() {
-		return movies;
-	}
 }
---
+++ src/main/java/eu/zeroware/mediafolderbutler/movie/entity/Ennelogia.java
@@ -0,0 +1,16 @@
+package eu.zeroware.mediafolderbutler.movie.entity;
+
+import eu.zeroware.mediafolderbutler.entity.Collection;
+
+
+public class Ennelogia extends Collection {
+	
+	// TODO ovviamente devo trovare un nome a questo oggetto!
+	
+	@Override
+	public String toString() {
+		String toString = super.toString();
+		return toString;
+	}
+
+}
