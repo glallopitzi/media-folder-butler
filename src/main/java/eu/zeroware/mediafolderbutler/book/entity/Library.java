b43faa01c9f19cc248aca0a7deaeff4c66a4b626
glallopitzi <giancarlolallopizzi@gmail.com>
2013-12-03 23:46:06 +0100

    book entities refactoring

--- src/main/java/eu/zeroware/mediafolderbutler/book/entity/Library.java
+++ src/main/java/eu/zeroware/mediafolderbutler/book/entity/Library.java
@@ -1,159 +1,18 @@
 package eu.zeroware.mediafolderbutler.book.entity;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
 
-public class Library {
+public class Library extends eu.zeroware.mediafolderbutler.entity.Library {
 
 	private static Logger logger = LogManager.getLogger(Library.class);
 	
-	private List<Writer> writers;
-	private List<Album> albums;
-	private List<Book> books;
-	
-	private List<String> genres;
-	private List<String> tags;
-
-	private List<Book> duplicateTracks;
-	private List<Book> unknowTracks;
-	
-	public Library() {
-		writers = new ArrayList<Writer>();
-		albums = new ArrayList<Album>();
-		books = new ArrayList<Book>();
-		duplicateTracks = new ArrayList<Book>();
-		unknowTracks = new ArrayList<Book>();
-	}
-
-	
-	public void addTrackToLibrary(Book book) {
-		addSongToLibrary(book);
-		addAlbumToLibrary(book);
-		addArtistToLibrary(book);
-	}
-
-	
-	
-	private void addSongToLibrary(Book book) {
-		if(!books.contains(book)){
-			books.add(book);
-			logger.info("Song: " + book.getName() + " added to Library");
-
-			
-		}else{
-			duplicateTracks.add(book);
-		}
-	}
-
-	private void addArtistToLibrary(Book book) {
-		Writer writer = new Writer(book);
-		if (!writers.contains(writer)) {
-			writers.add(writer);
-			logger.info("Artist: " + writer.getName() + " added to Library");
-			
-			
-		}else{
-			writers.get(writers.indexOf(writer)).addSong(book);
-		}
-	}
-
-	private void addAlbumToLibrary(Book book) {
-		Album album = new Album(book);
-		if (!albums.contains(album)) {
-			albums.add(album);
-			logger.info("Album: " + album.getName() + " added to Library");
-			
-			
-		}else{
-			albums.get(albums.indexOf(album)).addSong(book);
-		}
-	}
-
-	
 	@Override
 	public String toString() {
-		StringBuilder sb = new StringBuilder();
-		sb.append("\nLibrary details:\n");
-		sb.append(" - artists found: " + writers.size() + "\n");
-		for (Writer writer : writers) {
-			sb.append(writer);
-		}
-		sb.append("\n=======\n");
-		
-		sb.append(" - albums found: " + albums.size() + "\n");
-		for (Album album : albums) {
-			sb.append(album);
-		}
-		return sb.toString();
-	}
-
-	
-	
-	
-	
-	
-	
-	
-	
-	// getters and setters
-	public List<Writer> getArtists() {
-		return writers;
-	}
-
-	public void setArtists(List<Writer> writers) {
-		this.writers = writers;
-	}
-
-	public List<Album> getAlbums() {
-		return albums;
-	}
-
-	public void setAlbums(List<Album> albums) {
-		this.albums = albums;
-	}
-
-	public void setGenres(List<String> genres) {
-		this.genres = genres;
-	}
-
-	public List<String> getGenres() {
-		return genres;
-	}
-
-	public void setTags(List<String> tags) {
-		this.tags = tags;
-	}
-
-	public List<String> getTags() {
-		return tags;
-	}
-
-	public void setSongs(List<Book> books) {
-		this.books = books;
-	}
-
-	public List<Book> getSongs() {
-		return books;
-	}
-
-	public void setDuplicateTracks(List<Book> duplicateTracks) {
-		this.duplicateTracks = duplicateTracks;
-	}
-
-	public List<Book> getDuplicateTracks() {
-		return duplicateTracks;
-	}
-
-	public void setUnknowTracks(List<Book> unknowTracks) {
-		this.unknowTracks = unknowTracks;
-	}
-
-	public List<Book> getUnknowTracks() {
-		return unknowTracks;
+		logger.debug("Book library toString");
+		String toString = super.toString();
+		return toString;
 	}
 
 }
--- src/main/java/eu/zeroware/mediafolderbutler/book/entity/Writer.java
+++ src/main/java/eu/zeroware/mediafolderbutler/book/entity/Writer.java
@@ -1,123 +1,29 @@
 package eu.zeroware.mediafolderbutler.book.entity;
 
-import java.util.ArrayList;
-import java.util.List;
+import eu.zeroware.mediafolderbutler.entity.Artist;
 
-import eu.zeroware.mediafolderbutler.utils.rest.musicbrainz.search.ArtistSearchResultItem;
-
-public class Writer {
-	private String name;
-	private List<Album> albums;
-	private List<Book> books;
+public class Writer extends Artist {
 	
-	private List<String> genres;
-	private List<String> tags;
-	
-	private String mbid;
-	
-	public Writer() {
-	}
+	public Writer() {}
 
 	public Writer(Book book) {
-		this.name = book.getArtist();
-		books = new ArrayList<Book>();
-		books.add(book);
-		albums = new ArrayList<Album>();
-		albums.add(new Album(book.getAlbum(), book.getArtist()));
+		super(book);
 	}
 	
 	public Writer(String name) {
-		this.name = name;
+		super(name);
 	}
 
-	public Writer(ArtistSearchResultItem artistSearchResultItem) {
-		this.name = artistSearchResultItem.getName();
-	}
-
-	public void addSong(Book book){
-		if(!books.contains(book)){
-			books.add(book);
-		}
-		
-		addAlbum(book);
+	public void addBook(Book book){
+		super.addCreation(book);
 	}
 	
-	public void addAlbum(Book book){
-		Album album = new Album(book.getAlbum(), book.getArtist());
-		if(!albums.contains(album)){
-			albums.add(album);
-		}
-	}
-	
-	@Override
-	public boolean equals(Object obj) {
-		Writer o = (Writer) obj;
-		try {
-			return this.name.equals(o.getName());
-		} catch (Exception e) {
-			// logger.warn
-			return false;
-		}
-
-	}
 	
 	@Override
 	public String toString() {
-		StringBuilder builder = new StringBuilder();
-		builder.append("Artist info" + "\n");
-		builder.append("------------" + "\n");
-		builder.append("Name: " + name + "\n");
-		if(albums != null)
-			builder.append("Albums number: " + albums.size() + "\n");
-		builder.append("------------" + "\n");
-		return builder.toString();
+		String toString = super.toString();
+		// TODO replace something here?
+		return toString;
 	}
 
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public List<Album> getAlbums() {
-		return albums;
-	}
-
-	public void setAlbums(List<Album> albums) {
-		this.albums = albums;
-	}
-
-	public List<String> getTags() {
-		return tags;
-	}
-
-	public void setTags(List<String> tags) {
-		this.tags = tags;
-	}
-
-	public void setGenres(List<String> genres) {
-		this.genres = genres;
-	}
-
-	public List<String> getGenres() {
-		return genres;
-	}
-
-	public void setMbid(String mbid) {
-		this.mbid = mbid;
-	}
-
-	public String getMbid() {
-		return mbid;
-	}
-
-	public void setSongs(List<Book> books) {
-		this.books = books;
-	}
-
-	public List<Book> getSongs() {
-		return books;
-	}
 }
